7be2da92e22deefba590f2873bdee03ddac08523
IT-BRYYY <178906422+IT-BRYYY@users.noreply.github.com>
2026-08-18 23:16:48 +0800

    feat: add SoftwareEngineer controller

---
+++ src/main/java/com/bryancode/springboot/SoftwareEngineerController.java
@@ -0,0 +1,32 @@
+package com.bryancode.springboot;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
+public class SoftwareEngineerController {
+
+    private final SoftwareEngineerService softwareEngineerService;
+
+    public SoftwareEngineerController(SoftwareEngineerService softwareEngineerService) {
+        this.softwareEngineerService = softwareEngineerService;
+    }
+
+    @GetMapping
+    public List<SoftwareEngineer> getSoftwareEngineers() {
+        return softwareEngineerService.getAllSoftwareEngineers();
+    }
+
+    @GetMapping({"{id}"})
+    public SoftwareEngineer getSoftwareEngineersById(@PathVariable Integer id) {
+        return softwareEngineerService.getSoftwareEngineersById(id);
+    }
+
+    @PostMapping
+    public void addNewSoftwareEngineer(@RequestBody SoftwareEngineer softwareEngineer) {
+        softwareEngineerService.addNewSoftwareEngineer(softwareEngineer);
+    }
+}
